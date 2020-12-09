bf83b3f2824ad8fdb83dfc3cfdefd2e961526d9f
tantemac <hege.bjorkedal@gmail.com>
2020-12-09 15:18:58 +0100

    favourites

--- js/favourites.js
+++ js/favourites.js
@@ -1,36 +1,27 @@
 import { getExistingFavs } from "./utils/getFavourites.js";
 
-const favourites = getExistingFavs();
-
-const articleContainer = document.querySelector(".apiContainer");
-
-if (favourites.length === 0) {
-    articleContainer.innerHTML = "You have no favourites yet.";
-}
-
-favourites.forEach((favourite) => {
-
-    articleContainer.innherHTML += `<div class="article">
-                                    <h4>${favourite.title}</h4>
-                                    <p>Author: ${favourite.author}</p>
-                                    <p class="summarytitle">Summary:</p>
-                                    <p class="summary">${favourite.summary}</p>
-                                    <i class="far fa-heart" data-id="${favourite.id}" data-title="${favourite.title}"></i>
-                                    </div>`
-                                    
-});
-
+function renderFavourites() {
+    const favourites = getExistingFavs();
     
-    // let cssClass = "far";
+    const articleContainer = document.querySelector(".apiContainer");
+    
+    if (favourites.length === 0) {
+        articleContainer.innerHTML = "You have no favourites yet.";
+    }
+    
+    favourites.forEach((favourite) => {
 
-    // const doesObjectExist = favourites.find(function (fav) {
-    //     console.log(fav);
+        articleContainer.innerHTML += `<div class="article">
+                                        <h4>${favourite.title}</h4>
+                                        <p>Author: ${favourite.author}</p>
+                                        <p class="summarytitle">Summary:</p>
+                                        <p class="summary">${favourite.summary}</p>
+                                        <i class="fas fa-heart" data-id="${favourite.id}" data-title="${favourite.title}"></i>
+                                        </div>`
+                                        
+    });
+}
+    
+renderFavourites();
+    
 
-    //     return parseInt(fav.id) === favourite.id;
-    // });
-
-    // console.log (doesObjectExist);
-
-    // if (doesObjectExist) {
-    //     cssClass = "fas";
-    // }
