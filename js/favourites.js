e72cac296152e02efc22fc6356fdfbf64dfde1bc
tantemac <hege.bjorkedal@gmail.com>
2020-12-09 15:01:30 +0100

    favourites

--- js/favourites.js
+++ js/favourites.js
@@ -2,19 +2,35 @@ import { getExistingFavs } from "./utils/getFavourites.js";
 
 const favourites = getExistingFavs();
 
-const articleContainer = document.querySelector(".favouritesContainer");
+const articleContainer = document.querySelector(".apiContainer");
 
 if (favourites.length === 0) {
     articleContainer.innerHTML = "You have no favourites yet.";
 }
 
-favourites.forEach(favourite) => {
+favourites.forEach((favourite) => {
+
     articleContainer.innherHTML += `<div class="article">
-                                    <h4>${article.title}</h4>
-                                    <p>Author: ${article.author}</p>
+                                    <h4>${favourite.title}</h4>
+                                    <p>Author: ${favourite.author}</p>
                                     <p class="summarytitle">Summary:</p>
-                                    <p class="summary">${article.summary}</p>
-                                    <i class="${cssClass} fa-heart" data-id="${article.id}" data-title="${article.title}"></i>
+                                    <p class="summary">${favourite.summary}</p>
+                                    <i class="far fa-heart" data-id="${favourite.id}" data-title="${favourite.title}"></i>
                                     </div>`
                                     
-}
+});
+
+    
+    // let cssClass = "far";
+
+    // const doesObjectExist = favourites.find(function (fav) {
+    //     console.log(fav);
+
+    //     return parseInt(fav.id) === favourite.id;
+    // });
+
+    // console.log (doesObjectExist);
+
+    // if (doesObjectExist) {
+    //     cssClass = "fas";
+    // }
--- js/script.js
+++ js/script.js
@@ -6,14 +6,21 @@ const articleContainer = document.querySelector(".apiContainer");
 
 const favourites = getExistingFavs();
 
-(async function () {
+const getDataFromAPI = async () => {
     try {
         const response = await fetch(articleUrl);
         const json = await response.json();
+        createHTML(json);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-        articleContainer.innerHTML ="";
+function createHTML(json) {
 
-        json.forEach(function (article) {
+    articleContainer.innerHTML = "";
+
+    json.forEach(function (article) {
 
             let cssClass = "far";
 
@@ -28,19 +35,15 @@ const favourites = getExistingFavs();
             if (doesObjectExist) {
                 cssClass = "fas";
             }
-
-            articleContainer.innerHTML += `<div class="article">
-                                    <h4>${article.title}</h4>
-                                    <p>Author: ${article.author}</p>
-                                    <p class="summarytitle">Summary:</p>
-                                    <p class="summary">${article.summary}</p>
-                                    <i class="${cssClass} fa-heart" data-id="${article.id}" data-title="${article.title}"></i>
-                                    </div>`
-        });
-
-    } catch (error) {
-        console.log(error);
-    }
+            
+        articleContainer.innerHTML += `<div class="article">
+                                        <h4>${article.title}</h4>
+                                        <p>Author: ${article.author}</p>
+                                        <p class="summarytitle">Summary:</p>
+                                        <p class="summary">${article.summary}</p>
+                                        <i class="${cssClass} fa-heart" data-id="${article.id}" data-title="${article.title}"></i>
+                                        </div>`;
+  });
 
     const favButtons = document.querySelectorAll(".article i");
 
@@ -50,32 +53,121 @@ const favourites = getExistingFavs();
         button.addEventListener("click", handleClick);
     });
 
-    function handleClick() {
+}
 
-        this.classList.toggle("fas");
-        this.classList.toggle("far");
+function handleClick() {
 
-        const id = this.dataset.id;
-        const title = this.dataset.title;
+    this.classList.toggle("fas");
+    this.classList.toggle("far");
 
-        const currentFavs = getExistingFavs();
+    const id = this.dataset.id;
+    const title = this.dataset.title;
 
-        const articleExist = currentFavs.find(function (fav) {
-            return fav.id === id;
-        });
+    const currentFavs = getExistingFavs();
 
-        if (articleExist === undefined) {
-            const article = { id: id, title: title };
-            currentFavs.push(article);
-            saveFavs(currentFavs);
-        } else {
-            const newFavs = currentFavs.filter((fav) => fav.id !== id);
-            saveFavs(newFavs);
-        }
+    const articleExist = currentFavs.find(function (fav) {
+        return fav.id === id;
+    });
+
+    if (articleExist === undefined) {
+        const article = { id: id, title: title };
+        currentFavs.push(article);
+        saveFavs(currentFavs);
+    } else {
+        const newFavs = currentFavs.filter((fav) => fav.id !== id);
+        saveFavs(newFavs);
     }
+}
 
-    function saveFavs(favs) {
-        localStorage.setItem("favourites", JSON.stringify(favs));
-    }
+function saveFavs(favs) {
+    localStorage.setItem("favourites", JSON.stringify(favs));
+}
 
-})();
+getDataFromAPI();
+
+
+
+
+
+
+// import { baseUrl } from "./settings/api.js";
+// import { getExistingFavs } from "./utils/getFavourites.js";
+
+// const articleUrl = baseUrl + "articles";
+// const articleContainer = document.querySelector(".apiContainer");
+
+// const favourites = getExistingFavs();
+
+// (async function () {
+//     try {
+//         const response = await fetch(articleUrl);
+//         const json = await response.json();
+
+//         articleContainer.innerHTML ="";
+
+//         json.forEach(function (article) {
+
+//             let cssClass = "far";
+
+//             const doesObjectExist = favourites.find(function (fav) {
+//                 console.log(fav);
+
+//                 return parseInt(fav.id) === article.id;
+//             });
+
+//             console.log (doesObjectExist);
+
+//             if (doesObjectExist) {
+//                 cssClass = "fas";
+//             }
+
+//             articleContainer.innerHTML += `<div class="article">
+//                                     <h4>${article.title}</h4>
+//                                     <p>Author: ${article.author}</p>
+//                                     <p class="summarytitle">Summary:</p>
+//                                     <p class="summary">${article.summary}</p>
+//                                     <i class="${cssClass} fa-heart" data-id="${article.id}" data-title="${article.title}"></i>
+//                                     </div>`
+//         });
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+//     const favButtons = document.querySelectorAll(".article i");
+
+//     console.log(favButtons);
+
+//     favButtons.forEach((button) => {
+//         button.addEventListener("click", handleClick);
+//     });
+
+//     function handleClick() {
+
+//         this.classList.toggle("fas");
+//         this.classList.toggle("far");
+
+//         const id = this.dataset.id;
+//         const title = this.dataset.title;
+
+//         const currentFavs = getExistingFavs();
+
+//         const articleExist = currentFavs.find(function (fav) {
+//             return fav.id === id;
+//         });
+
+//         if (articleExist === undefined) {
+//             const article = { id: id, title: title };
+//             currentFavs.push(article);
+//             saveFavs(currentFavs);
+//         } else {
+//             const newFavs = currentFavs.filter((fav) => fav.id !== id);
+//             saveFavs(newFavs);
+//         }
+//     }
+
+//     function saveFavs(favs) {
+//         localStorage.setItem("favourites", JSON.stringify(favs));
+//     }
+
+// })();
